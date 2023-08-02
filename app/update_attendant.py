f5a893668bb860a0ce06f1b16e2273d25f3cf1d9
Joao Victor <joaovictorsousadeoliveira345@gmail.com>
2023-08-02 16:33:30 -0300

    update attendant was tested and aproved for the code

--- app/update_attendant.py
+++ app/update_attendant.py
@@ -1,12 +1,11 @@
 import pandas as pd
 
 
+# Function that recieve a excel workbook and filter the columns based in 
 def atualiza_associado(planilha_excel):
     df = pd.read_excel(planilha_excel)
-
-    df_selected = df[['B', 'S']]
-
-    df_unique = df_selected.drop_duplicates(subset=['S'])
+    df_selected = df[['Proprietário do caso', 'E-mail do Proprietário']].copy()
+    df_unique = df_selected.drop_duplicates(subset=df_selected.columns[1])
 
     return df_unique
 

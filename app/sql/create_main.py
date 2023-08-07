91f76f38c4830ac8a3cf0689d9e3c1d89211540c
Joao Victor <joaovictorsousadeoliveira345@gmail.com>
2023-08-07 07:37:37 -0300

    feat: create_main code was added it drops all tables and creates new ones based on __all_models

---
+++ app/sql/create_main.py
@@ -0,0 +1,11 @@
+import os
+import sys
+app_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(app_path)
+
+from conf.db_session import create_table
+
+
+if __name__ == "__main__":
+    create_table()
+
